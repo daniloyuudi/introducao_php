e96fa0779ca42cb3113b24a5fac1418fe7ab3025
Danilo Yuudi <dnl496@outlook.com>
2020-11-19 16:02:11 -0300

    Final Avançando com OO

---
+++ avancando-oo/enderecos.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use Alura\Banco\Modelo\Endereco;
+
+require_once 'autoload.php';
+
+$umEndereco = new Endereco('Petrópolis', 'bairro qualquer', 'minha rua', '71B');
+$outroEndereco = new Endereco('Rio', 'Centro', 'Uma rua aí', '50');
+
+echo $umEndereco . PHP_EOL;
+echo $outroEndereco;
