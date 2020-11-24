ae42c9b99456326450dd7a3be1b08d4d83924c39
Danilo Yuudi <dnl496@outlook.com>
2020-11-24 17:21:36 -0300

    Inicio MVC

---
+++ mvc/gerenciador_de_cursos/src/Controller/Persistencia.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Infra\EntityManagerCreator;
+use Alura\Cursos\Entity\Curso;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    private $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        $descricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
+        $curso = new Curso();
+        $curso->setDescricao($descricao);
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+
+        echo "Curso $descricao salvo com sucesso";
+        header('Location: /listar-cursos', false, 302);
+    }
+}
