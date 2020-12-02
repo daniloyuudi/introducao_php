c1fece80a174074f13e89eff4fb14277f5d68d8e
Danilo Yuudi <dnl496@outlook.com>
2020-12-02 16:47:20 -0300

    Final curso PHPUnit

--- phpunit/teste-avaliador.php
+++ phpunit/teste-avaliador.php
@@ -7,29 +7,3 @@ use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
-
-// Arrange - Given
-$leilao = new Leilao('Fiat 147 0KM');
-
-$maria = new Usuario('Maria');
-$joao = new Usuario('João');
-
-$leilao->recebeLance(new Lance($joao, 2000));
-$leilao->recebeLance(new Lance($maria, 2500));
-
-$leiloeiro = new Avaliador();
-
-
-// Act - When
-$leiloeiro->avalia($leilao);
-
-$maiorValor = $leiloeiro->getMaiorValor();
-
-// Assert - Then
-$valorEsperado = 2500;
-
-if ($valorEsperado == $maiorValor) {
-    echo "TESTE OK";
-} else {
-    echo "TESTE FALHOU";
-}
